fa8e864fbf2a98bf8bce062bc7e970ac761e97eb
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-04-24 22:56:25 -0400

    Add files via upload

---
+++ bot/run_bot.py
@@ -0,0 +1,14 @@
+from aiogram import Bot, Dispatcher, executor, types
+from config import BOT_TOKEN
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.answer('Добро пожаловать в Start New Life Bot!')
+
+def run_bot():
+    from handlers import register_handlers
+    register_handlers(dp)
+    executor.start_polling(dp, skip_updates=True)
