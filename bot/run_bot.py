0fd57211528d327074c0fcbb757be2d9206e31ac
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-04-24 23:28:05 -0400

    Update run_bot.py

--- bot/run_bot.py
+++ bot/run_bot.py
@@ -1,5 +1,6 @@
 from aiogram import Bot, Dispatcher, executor, types
 from config import BOT_TOKEN
+from handlers.register_handlers import register_handlers
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
@@ -9,6 +10,5 @@ async def start_command(message: types.Message):
     await message.answer('Добро пожаловать в Start New Life Bot!')
 
 def run_bot():
-    from handlers import register_handlers
     register_handlers(dp)
     executor.start_polling(dp, skip_updates=True)
