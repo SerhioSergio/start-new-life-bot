d98194ed66902ae676616f50b7cb868ff0466c3d
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-05-05 09:16:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from bot.run_bot import run_bot
+from aiogram import Bot, Dispatcher, executor, types
+from handlers.money_block import register_handlers_money
+import config
 
-if __name__ == '__main__':
-    run_bot()
+bot = Bot(token=config.BOT_TOKEN)
+dp = Dispatcher(bot)
+
+# Регистрируем обработчики первой зоны
+register_handlers_money(dp)
+
+if name == '__main__':
+    executor.start_polling(dp, skip_updates=True)
