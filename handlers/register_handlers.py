5967f5b752e5f080ba2e370eead15c062ce69ab9
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-05-06 11:16:00 -0400

    Update register_handlers.py

--- handlers/register_handlers.py
+++ handlers/register_handlers.py
@@ -1,1 +1,21 @@
-# Регистрирует все зоны с 1 по 7
+def register_handlers(dp):
+    from handlers.money_block import register_money_block_handlers
+    from handlers.zone_1.start import register_zone_1_handlers
+    from handlers.zone_2.start import register_zone_2_handlers
+    from handlers.zone_3.start import register_zone_3_handlers
+    from handlers.zone_4.start import register_zone_4_handlers
+    from handlers.zone_5.start import register_zone_5_handlers
+    from handlers.zone_6.start import register_zone_6_handlers
+    from handlers.zone_7.start import register_zone_7_handlers
+
+    # Зона 1 — Деньги и самооценка
+    register_money_block_handlers(dp)
+
+    # Остальные зоны:
+    register_zone_1_handlers(dp)
+    register_zone_2_handlers(dp)
+    register_zone_3_handlers(dp)
+    register_zone_4_handlers(dp)
+    register_zone_5_handlers(dp)
+    register_zone_6_handlers(dp)
+    register_zone_7_handlers(dp)# Регистрирует все зоны с 1 по 7
