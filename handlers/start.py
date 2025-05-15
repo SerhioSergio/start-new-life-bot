4d52a8224a42e573e79f0ced561c25d7eece90a8
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-05-14 22:47:28 -0400

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,10 @@
 # Стартовая логика бота
+from aiogram.types import Message
+from aiogram.dispatcher import Dispatcher
+from handlers.zone_8.zone8 import start_day_for_zone8
+
+async def start_zone_8(message: Message, context):
+    await start_day_for_zone8(message, context)
+
+def register_zone_8_handlers(dp: Dispatcher):
+    pass  # здесь можно будет регистрировать хендлеры зоны 8
