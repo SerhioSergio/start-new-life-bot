f18af51de0f37d8870809222615b85400fc71908
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-05-14 23:44:58 -0400

    Update start.py

--- handlers/zone_8/start.py
+++ handlers/zone_8/start.py
@@ -5,5 +5,5 @@ from handlers.zone_8.zone8 import start_day_for_zone8
 async def start_zone_8(message: Message, context):
     await start_day_for_zone8(message, context)
 
-def register_zone_8(dp: Dispatcher):
-    pass
+def register_zone_8_handlers(dp: Dispatcher):
+    dp.register_message_handler(start_zone_8)
