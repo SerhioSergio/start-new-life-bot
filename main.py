cced1d289f56f1fe793d0eaec6e70bc5d4e4524e
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-05-15 00:30:48 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from aiogram import Bot, Dispatcher, executor
 from config import BOT_TOKEN
-from handlers.register_handlers import register_handlers
+import handlers.register_handlers as register_handlers
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 print("Бот запущен")
 
-if __name__ == "__main__":
+if name == "__main__":
     import asyncio
-    from handlers import register_handlers  # замени на свой модуль
     from telegram.ext import ApplicationBuilder
 
+    register_handlers.register_handlers(dp)
+
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-   register_handlers.register_handlers(app)
-    app.run_polling()  # именно polling, не webhook!
+    app.run_polling()
