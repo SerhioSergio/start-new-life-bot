748f3200759405ba76273b7d31c4479c040358b0
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-05-14 23:26:58 -0400

    Update zone_6.py

--- handlers/zone_6/zone_6.py
+++ handlers/zone_6/zone_6.py
@@ -1,7 +1,7 @@
 # Logic for zone_6
 from aiogram.types import Message
 from handlers.user_progress import save_user_step
-from ai_module import analyze_and_reply
+from handlers.ai_module import analyze_and_reply
 
 ZONE_NAME = "Зона 6: Перепрошивка и трансформация"
 STEPS = [
