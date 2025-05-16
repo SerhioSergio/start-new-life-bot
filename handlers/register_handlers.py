dd88e3f8e1bb3b0ea9c19f6e65577e2fbdb612f7
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-05-15 22:25:26 -0400

    Update register_handlers.py

--- handlers/register_handlers.py
+++ handlers/register_handlers.py
@@ -1,14 +1,14 @@
 def register_handlers(dp):
-from handlers.money_block import register_money_block_handlers
-from handlers.zone_1.start import register_zone_1_handlers
-from handlers.zone_2.start import register_zone_2_handlers
-from handlers.zone_3.start import register_zone_3_handlers
-from handlers.zone_4.start import register_zone_4_handlers
-from handlers.zone_5.start import register_zone_5_handlers
-from handlers.zone_6.start import register_zone_6_handlers
-from handlers.zone_7.start import register_zone_7_handlers
-from handlers.zone_8.start import register_zone_8_handlers
-from handlers.whisper_handler import register_voice_handler
+    from handlers.money_block import register_money_block_handlers
+    from handlers.zone_1.start import register_zone_1_handlers
+    from handlers.zone_2.start import register_zone_2_handlers
+    from handlers.zone_3.start import register_zone_3_handlers
+    from handlers.zone_4.start import register_zone_4_handlers
+    from handlers.zone_5.start import register_zone_5_handlers
+    from handlers.zone_6.start import register_zone_6_handlers
+    from handlers.zone_7.start import register_zone_7_handlers
+    from handlers.zone_8.start import register_zone_8_handlers
+    from handlers.whisper_handler import register_voice_handler
 
 
 def register_handlers(dp):
