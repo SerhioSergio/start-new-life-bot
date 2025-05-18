7d0b1679324b8a51e41b6d29f4c5ff0119040020
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-05-17 22:30:44 -0400

    Update register_handlers.py

--- handlers/register_handlers.py
+++ handlers/register_handlers.py
@@ -1,18 +1,15 @@
-def register_handlers(dp):
-    from handlers.money_block import register_money_block_handlers
-    from handlers.zone_1.start import register_zone_1_handlers
-    from handlers.zone_2.start import register_zone_2_handlers
-    from handlers.zone_3.start import register_zone_3_handlers
-    from handlers.zone_4.start import register_zone_4_handlers
-    from handlers.zone_5.start import register_zone_5_handlers
-    from handlers.zone_6.start import register_zone_6_handlers
-    from handlers.zone_7.start import register_zone_7_handlers
-    from handlers.zone_8.start import register_zone_8_handlers
-    from handlers.whisper_handler import register_voice_handler
-
+from handlers.money_block import register_money_block_handlers
+from handlers.zone_1.start import register_zone_1_handlers
+from handlers.zone_2.start import register_zone_2_handlers
+from handlers.zone_3.start import register_zone_3_handlers
+from handlers.zone_4.start import register_zone_4_handlers
+from handlers.zone_5.start import register_zone_5_handlers
+from handlers.zone_6.start import register_zone_6_handlers
+from handlers.zone_7.start import register_zone_7_handlers
+from handlers.zone_8.start import register_zone_8_handlers
+from handlers.whisper_handler import register_voice_handler
 
 def register_handlers(dp):
-    # Основные зоны
     register_money_block_handlers(dp)
     register_zone_1_handlers(dp)
     register_zone_2_handlers(dp)
@@ -22,6 +19,4 @@ def register_handlers(dp):
     register_zone_6_handlers(dp)
     register_zone_7_handlers(dp)
     register_zone_8_handlers(dp)
-
-    # Голосовые
     register_voice_handler(dp)
