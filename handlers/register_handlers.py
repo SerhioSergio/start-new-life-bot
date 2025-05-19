334cee09db961d1f4550e47a2610fa59be351647
SerhioSergio <90148550+SerhioSergio@users.noreply.github.com>
2025-05-18 22:33:27 -0400

    Update register_handlers.py

--- handlers/register_handlers.py
+++ handlers/register_handlers.py
@@ -1,3 +1,6 @@
+from aiogram import Dispatcher
+
+# Импорт всех функций регистрации хендлеров
 from handlers.money_block import register_money_block_handlers
 from handlers.zone_1.start import register_zone_1_handlers
 from handlers.zone_2.start import register_zone_2_handlers
@@ -9,8 +12,12 @@ from handlers.zone_7.start import register_zone_7_handlers
 from handlers.zone_8.start import register_zone_8_handlers
 from handlers.whisper_handler import register_voice_handler
 
-def register_handlers(dp):
+# Общая функция регистрации всех хендлеров
+def register_handlers(dp: Dispatcher):
+    # Базовый хендлер на /start (из money_block)
     register_money_block_handlers(dp)
+
+    # Остальные зоны
     register_zone_1_handlers(dp)
     register_zone_2_handlers(dp)
     register_zone_3_handlers(dp)
@@ -19,4 +26,6 @@ def register_handlers(dp):
     register_zone_6_handlers(dp)
     register_zone_7_handlers(dp)
     register_zone_8_handlers(dp)
+
+    # Голосовые
     register_voice_handler(dp)
